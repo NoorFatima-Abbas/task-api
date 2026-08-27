afe76c60216350e366b34a4a712d0b917a0b4892
nfatimabscs24seecs-prog <191664991+NoorFatimaAbbas@users.noreply.github.com>
2026-08-27 11:35:59 +0500

    Stage 1:root and health endpoints

--- main.py
+++ main.py
@@ -3,4 +3,7 @@ app=FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello, server"}
+    return {"name":"Task API", "version":"1.0", "endpoints":["/tasks"]}
+@app.get("/health")
+def read_health():
+    return {"status":"ok"}
