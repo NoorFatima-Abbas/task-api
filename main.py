f115897a7e7c596f0c812fe62740806bef3cd0b0
nfatimabscs24seecs-prog <191664991+NoorFatimaAbbas@users.noreply.github.com>
2026-08-28 10:52:27 +0500

    Stage 4:full CRUD

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 from fastapi import Request
 from fastapi.exceptions import RequestValidationError
 
+# for the POST updates in CRUD
 class TaskCreate(BaseModel):
    title:str
+
+# for the PUT(update) in CRUD
+class TaskUpdate(BaseModel):
+   title:str|None=None
+   done:bool|None=None
    
 app=FastAPI()
 
@@ -52,3 +58,41 @@ def create_task(task:TaskCreate):
 def validation_error_handler(request:Request, exc:RequestValidationError):
    return JSONResponse(status_code=400,content={"error":"Title is required and cannot be empty."})
 
+@app.put("/tasks/{task_id}")
+def update_task(task_id: int,task_update:TaskUpdate ):
+    # 1. Find the task
+    found_task = None
+    for task in tasks:
+        if task["id"] == task_id:
+            found_task = task
+            break
+
+    # 2. Handle 404 outside the loop
+    if not found_task:
+        return JSONResponse(
+            status_code=404, content={"error": f"Task {task_id} not found"}
+        )
+    # Validate title if it was provided
+    if task_update.title is not None:
+       if not task_update .title.strip():
+          return JSONResponse(status_code=400, content={"error":"Title cannot be empty"})
+       found_task["title"]=task_update.title
+
+       #Update done status it it was provided
+    if task_update.done is not None:
+          found_task["done"]=task_update.done
+    return found_task
+
+@app.delete("/tasks/{task_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_task(task_id:int):
+    #Loop to find the task
+    for task in tasks:
+        if task["id"]==task_id:
+            tasks.remove(task)
+            return None       #204 response with an empty body
+
+    #if loop finishes without returning , task wasn't found
+    return JSONResponse(
+        status_code=status.HTTP_404_NOT_FOUND,
+        content={"error":f"Task {task_id} not found"}
+    )
